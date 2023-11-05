d4694727398756fd1fb4ec7f1e2fd62df1100d39
QUOCVIET <viet7891@gmail.com>
2023-11-05 16:02:01 +0700

    create project

---
+++ src/main/java/com/bookingcare/BookingcareApplication.java
@@ -0,0 +1,13 @@
+package com.bookingcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingcareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingcareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookingcare/BookingcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookingcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
